0116e51702f5edc5b37cbd914678e0fbd17b70dc
Ivy <mayanhu@sheridancollege.ca>
2019-01-25 14:23:44 -0500

    Card file created

---
+++ src/card/Carddealer.java
@@ -0,0 +1,31 @@
+/*
+    Name: Yan Hui Ma (Ivy)
+    Assignment: 
+    Program: 
+    Date: 
+
+    Description: 
+
+*/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package card;
+
+/**
+ *
+ * @author rebeccakong
+ */
+public class Carddealer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Card newSuit = new Card();
+    }
+
+}
